c41fff03b21ca177cd4aba47e6ca3d27979ab57a
chenjun <chenjun@promote.cache-dns.local>
2016-02-01 16:49:33 +0800

    添加ajax模拟功能

--- examples/django/build/static/js/index.js
+++ examples/django/build/static/js/index.js
@@ -1,4 +1,15 @@
-var x = 22;
-var y = 44;
+$(function() {
+    $('.ajax_btn').on('click', function() {
+        $.ajax({
+            url: '/ttdiscuss/concern/more/',
+            method: 'POST',
+            data: {},
+            success: function(data) {
+                console.log(data.students);
+            },
+            error: function() {
 
-console.log(x + y);
+            }
+        });
+    });
+});
